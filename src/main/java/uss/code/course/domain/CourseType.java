ceed0a975e3e00d492c85ed866c28be0c7645727
xunxxoie <ahh0520@naver.com>
2026-02-09 20:36:33 +0900

    fix: 2026 수업유형 반영

--- src/main/java/uss/code/course/domain/CourseType.java
+++ src/main/java/uss/code/course/domain/CourseType.java
@@ -14,12 +14,18 @@ public enum CourseType {
     ART_PRACTICE("미술실기"),
     ARTS_PHYSICAL_PRACTICE("예술체육실기"),
     E_LEARNING("e-Learning"),
+    E_LEARNING_HUSS("e-Learning(HUSS)"),
     ONLINE_BLENDED("온라인혼합형강좌"),
+    ONLINE_BLENDED_HUSS("온라인혼합형강좌(HUSS)"),
+    FIELD_TYPE_HUSS("현장형(HUSS)"),
     K_MOOC("K-MOOC"),
     OCU("열린사이버대학(OCU)"),
     SOCIAL_SERVICE_1("사회봉사(1)"),
     SOCIAL_SERVICE_2("사회봉사(2)"),
-    SOCIAL_SERVICE_3("사회봉사(3)");
+    SOCIAL_SERVICE_3("사회봉사(3)"),
+    RISE_WITHOUT_SCHEDULE("RISE(시간표 없음)"),
+    RISE_WITH_SCHEDULE("RISE(시간표 있음)"),
+    SELF_DESIGNED_SEMINAR("자기설계세미나");
 
     private final String name;
 }
